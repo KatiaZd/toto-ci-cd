ae5b250add9c8243966507ccba43bc49b68988ef
Katia ZOUAD <katia.zouad@hotmail.fr>
2025-11-03 14:42:51 +0100

    first commit

---
+++ toto-presentation/src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      locale: import.meta.env.VITE_I18N_LOCALE,
+      url: import.meta.env.VITE_BACKEND_URL,
+
+    };
+  },
+};
+  
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Current locale: {{ locale }}</p>
+    <p>Backend URL: {{ url }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
